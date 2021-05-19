48ff6f36569919bdf90d9e23561ce3d35f66e941
Philippe Beck <philippe@philippebeck.net>
2021-05-19 22:10:20 +0200

    Update Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <section class="home">
     <h1>This is the home page</h1>
 
+    <Head text="This is a Head Example of a Vue Component !"/>
+
     <figure>
       <img alt="Vue logo" src="../assets/logo.png">
       <figcaption>
@@ -11,19 +13,24 @@
 
         <p>The Progressive JavaScript Framework</p>
       </figcaption>
-    </figure>   
+    </figure>
 
+    <!-- <Canvas/> -->
   </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import Head from '@/components/Head.vue'
 import Link from '@/components/Link.vue'
+//import Canvas from '@/components/Canvas.vue'
 
 export default {
   name: 'Home',
   components: {
-    Link
+    Head,
+    Link//,
+    //Canvas
   }
 }
 </script>

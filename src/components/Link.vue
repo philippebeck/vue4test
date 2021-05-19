eec2d6bcb4b3ee57a8fc8992d8c1bae2bf385eba
Philippe Beck <philippe@philippebeck.net>
2021-05-19 19:36:46 +0200

    Create Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,30 @@
+<template>
+  <a :href="url"
+    :title="title">
+    {{ text }}
+  </a>
+</template>
+
+<script>
+  export default {
+    name: "Link",
+    props: ["url", "title", "text"]
+  }
+</script>
+
+<style lang="scss" scoped>
+  a {
+    --link-color: var(--primary);
+    --link-text-decoration: none;
+
+    color: var(--link-color);
+    text-decoration: var(--link-text-decoration);    
+  }
+
+  a:hover,
+  a:focus {
+    --link-color-hover: var(--violet);
+    
+    color: var(--link-color-hover);
+  }
+</style>

3842bca4800923fc1d713f6543206f20b0915d8b
Philippe Beck <philippe@philippebeck.net>
2021-05-19 20:11:24 +0200

    Update Home generated for Link test

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+
+    <figure>
+      <img alt="Vue logo" src="../assets/logo.png">
+      <figcaption>
+        <Link url="https://vuejs.org"
+          title="Go to Vue Website"
+          text="Vue"/>
+
+        <p>The Progressive JavaScript Framework</p>
+      </figcaption>
+    </figure>   
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Link from '@/components/Link.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Link
+  }
+}
+</script>

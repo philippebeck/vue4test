5dfd70fc2f6d8aea1377126f4115aa01921736c0
Philippe Beck <philippe@philippebeck.net>
2021-05-23 10:55:36 +0200

    Create Img component

---
+++ src/components/Img.vue
@@ -0,0 +1,22 @@
+<template>
+    <img :src="src" :alt="alt">
+</template>
+
+<script>
+  export default {
+    name: "Img",
+    props: ["src", "alt"]
+  }
+</script>
+
+<style lang="scss" scoped>
+    img {
+        --img-max-width: 100%;
+        --img-height: auto;
+        --img-object-fit: contain;
+
+        max-width: var(--img-max-width);
+        height: var(--img-height);
+        object-fit: var(--img-object-fit);
+    }
+</style>

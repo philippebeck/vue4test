d6c2b8c2a8b4aa034081c044e669e614a26dbf5f
Philippe Beck <philippe@philippebeck.net>
2021-05-24 00:08:27 +0200

    Finish Img component

--- src/components/Img.vue
+++ src/components/Img.vue
@@ -1,22 +1,36 @@
+<!------------------------->
+<!-- Img Basic Component -->
+<!------------------------->
+
 <template>
-    <img :src="src" :alt="alt">
+  <img :src="src" 
+    :alt="alt">
 </template>
 
 <script>
   export default {
     name: "Img",
-    props: ["src", "alt"]
+    props: {
+      src: {
+        type: String,
+        required: true
+      },
+      alt: {
+        type: String,
+        required: true
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-    img {
-        --img-max-width: 100%;
-        --img-height: auto;
-        --img-object-fit: contain;
+  img {
+    --img-max-width: 100%;
+    --img-height: auto;
+    --img-object-fit: contain;
 
-        max-width: var(--img-max-width);
-        height: var(--img-height);
-        object-fit: var(--img-object-fit);
-    }
+    max-width: var(--img-max-width);
+    height: var(--img-height);
+    object-fit: var(--img-object-fit);
+  }
 </style>

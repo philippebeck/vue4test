bd327e6c476fff8bdc51e493e57a5e36915c1051
Philippe Beck <philippe@philippebeck.net>
2021-05-26 13:16:58 +0200

    Create Quote component

---
+++ src/components/basic/Quote.vue
@@ -0,0 +1,75 @@
+<!-- ----------------------- -->
+<!-- Quote : basic component -->
+<!-- ----------------------- -->
+
+<template>
+  <figure v-if="isBlock === true">
+    
+    <blockquote :cite="url">
+      <slot></slot>
+      {{ quote }}
+    </blockquote>
+
+    <figcaption v-if="info">
+
+      <Link :url="url"
+        :info="info">
+        <cite>
+          {{ info }}
+        </cite>
+      </Link>
+
+    </figcaption>
+  </figure>
+
+  <q v-else
+    :cite="url">
+    <slot></slot>
+    {{ quote }}
+  </q>
+</template>
+
+<script>
+  import Link from "@/components/basic/Link.vue"
+
+  export default {
+    name: "Quote",
+    components: {
+      Link
+    },
+    props: {
+      quote: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String
+      },
+      info: {
+        type: String
+      },
+      content: {
+        type: String
+      },
+      isBlock: {
+        type: Boolean
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  figure {
+    margin: 0;
+  }
+
+  blockquote {
+    display: inline-block;
+  }
+
+  q,
+  blockquote {
+    padding: 10px 20px;
+    background-color: var(--secondary);
+  }
+</style>
